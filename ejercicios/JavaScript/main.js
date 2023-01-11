57efd6e41dd066b15bae83e6e2a1f3b095b3dfbe
JV4NN <rabjuan@outlook.com>
2023-01-11 08:39:08 -0600

    Cambio de nombre CarpetaHenry por carpetacurso

---
+++ ejercicios/JavaScript/main.js
@@ -0,0 +1,25 @@
+var nombre = "juan";
+var edad = 30;
+var altura = 1.90;
+var casado = true;
+var pregunta = 20 < 10;
+
+console.log(nombre, edad, altura,casado, pregunta,);
+var contador = 1;
+do{
+    console.log(contador);
+    contador++;
+    } while (contador <=10);
+
+/*var contador =1;
+while(contador<= 10){
+    console.log(contador);
+    contador = contador +1;
+}
+*/
+
+/*for (var contador = 100;contador >=1; contador--){
+    console.log(contador);
+}*/
+
+console.log("Aqui termina el programa")
